f72e57abae6a684b98a4667763aa6866245eb0b4
henkeh83 <henkeh83@gmail.com>
2017-02-03 10:23:53 +0100

    Added carousel and fixed header

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+var items = 0;
+
 $(document).ready(function(){
   $(".hidden").fadeIn('slow','swing').removeClass('hidden');
+  itemsCount();
+
+  $("#searchBtn").click(function(){
+      $("#secondView,#firstView").toggle();
+  });
+
+  function itemsCount(){
+    $("#itemsShow").html(items);
+  }
+
 });
