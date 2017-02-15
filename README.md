6878d50e86eda2bdcf7da63b746f63dc93f5c045
henkeh83 <henkeh83@gmail.com>
2017-02-15 12:11:13 +0100

    Maybe final push

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-Schoolproject for weeks 5-7 2017.
+Auther: Henrik Hagström.
+Course: Webbutveckling.
+Class: SJK16G.
+School: Jensen yrkeshögskola Göteborg.
+https://snacksonline.github.io
 
-Creating a website where you can find snacks near you :)
+https://www.linkedin.com/?utm_source=operabrowser&utm_medium=speeddial&utm_campaign=desktop
+
+A website where you can find for stores near you that contain the snacks you search for.
+
+Technics used in project:
+* HTML
+* CSS
+* JavaScript
+* jquery
+* bootstrap
+* AJAX
+* json
+
+My approach was web first and my purpose was to learn web development.
+Considering my level of (or lack of) experience I am quite pleased with the result.
+Done today I would have started with mobile first.
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-1
 //var urlForShops = "http:mardby.se/AJK15G/animals_json.php?";
 var items = 0;
 
@@ -6,8 +5,6 @@ var items = 0;
 $(document).ready(function() {
     fadeStuffs();
     itemsCount();
-
-
 
     /* Buttons for the shop progression
      */
@@ -17,7 +14,12 @@ $(document).ready(function() {
 
 
     $("#shopTable").on("click", ".shop", function() {
+        var choosenShop = $(this).attr('id');
+        console.log(choosenShop);
         $("#secondView,#thirdView").toggle();
+        $.getJSON("http:mardby.se/AJK15G/animals_json.php?animalId=" + choosenShop, function(data) {
+            $("#theMall").append('<div class="Shop"><div class="shopItem"><img style="width: 140px;height: 140px;border-radius:10%" src="' + data.animal.img_src + '"></div><div class="numbers"><p class="price">' + data.animal.name + '</P><input class="quantity" type="number"><button class="add"><i class="fa fa-cart-plus" aria-hidden="true"></i></button></div></div>')
+        });
     });
 
 
@@ -27,21 +29,6 @@ $(document).ready(function() {
         }
     });
 
-    /* All the buttons for switching between admin
-      sites.
-    */
-
-    $("#loggaIn").click(function() {
-        if ($('#firstView').css('display') == 'block') {
-            $("#adminLog,#firstView").toggle();
-        } else if ($('#secondView').css('display') == 'block') {
-            $('#adminLog,#secondView').toggle();
-        } else if ($('#thirdView').css('display') == 'block') {
-            $('#adminLog,#thirdView').toggle();
-        } else if ($('#forthView').css('display') == 'block') {
-            $('#adminLog,#forthView').toggle();
-        }
-    });
 
     $("#confirmLogBtn").click(function() {
         // kolla namn och lösen
@@ -49,74 +36,47 @@ $(document).ready(function() {
         $("#loggaIn,#loggaUt").toggle();
     });
 
-    // Checks wich "section" the user is currently in and changes it to the main page.
-    $("#loggaUt").click(function() {
-        if ($("#adminMain").css('display') == 'block') {
-            $('#adminMain,#firstView').toggle();
-        } else if ($('#adminButik').css('display') == 'block') {
-            $('#adminButik,#firstView').toggle();
-        } else if ($('#adminSnacks').css('display') == 'block') {
-            $('#adminSnacks,#firstView').toggle();
-        } else if ($('#shopRemoval').css('display') == 'block') {
-            $('#shopRemoval,#firstView').toggle();
-        } else if ($('#snacksRemoval').css('display') == 'block') {
-            $('#snacksRemoval,#firstView').toggle();
-        }
-        $('#loggaUt,#loggaIn').toggle();
-    });
-
-    $('#toShops').click(function() {
-        $('#adminMain,#adminButik').toggle();
-    });
-
-    $('#toSnacks').click(function() {
-        $('#adminMain,#adminSnacks').toggle();
-    });
-
-    $('#removeShop').click(function() {
-        $('#adminMain,#shopRemoval').toggle();
-    });
-
-    $('#removeSnack').click(function() {
-        $('#adminMain,#snacksRemoval').toggle();
-    });
-
-    $("#saveShop").click(function() {
-        $("#adminButik,#adminMain").toggle();
-    });
-
-    $("#saveSnacks").click(function() {
-        $("#adminSnacks,#adminMain").toggle();
-    });
-
-    $('#deleteSnack').click(function() {
-        $('#adminMain,#snacksRemoval').toggle();
-    });
-
-    $('#deleteShop').click(function() {
-        $('#adminMain,#shopRemoval').toggle();
-    });
 
     /* AJAX functions.
      */
 
-    // Test AJAX funktion, används som mall
-    $('#testbtn').click(function() {
-        $.ajax({
-            type: 'GET',
-            url: 'http://date.jsontest.com',
-            success: function(dbStuff) {
-                //    $.each(dbStuff, function(i, stuffStuff){   Använd denna sedan när du får array
-                $("#theMall").append('<div class="Shop"><div class="shopItem"><p>' + dbStuff.time + '</p></div><div class="numbers"><p class="price">' + dbStuff.date + '</P><input class="quantity" type="number"><button class="add"><i class="fa fa-cart-plus" aria-hidden="true"></i></button></div></div>')
-                //    });
+    // "https://learnwebcode.github.io/json-example/animals-1.json" awesome url for JSON testing.
+
+    /* Loads up shops to choose from
+      todo: det kommer bli problem när man faktiskt kan ta bort shops, men vi kommer nog inte dit :)
+    */
+    $.getJSON("https://learnwebcode.github.io/json-example/animals-1.json", function(data) {
+        for (i = 0; i < data.length; i++) {
+            console.log(data[i].name);
+            $(".shopShopSelect").append('<option class="shopListOfShops" value="' + data[i].name + '">' + data[i].name + '</option>');
+            $("#snackShopSelector").append('<option class="snackListOfShops" value="' + data[i].name + '">' + data[i].name + '</option>');
+        }
+    });
+
+    /* Fills the snacks list with the candy thats in the currently choosen shop.
+     */
+    $("#snackShopSelector").change(function() {
+        $("#snackSelect").html('');
+        var choosenName = $("#snackShopSelector option:selected").text();
+        $.getJSON("https://learnwebcode.github.io/json-example/animals-1.json", function(data) {
+            for (i in data) {
+                if (data[i].name == choosenName) {
+                    for (j = 0; j < data[i].foods.likes.length; j++) {
+                        $("#snackSelect").append("<option>" + data[i].foods.likes[j] + "</option>");
+                        console.log("what is goin on?");
+                    }
+                }
             }
         });
     });
 
+
+
     // Function for displaying shops that hold checked candy
     function appendShop(urlForShops, classTag) {
         $.getJSON(urlForShops, function(shopData) {
-            $('#shopTable').append('<a href="#"><div class="shop ' + classTag + '" ><h3>' + shopData.animal.name + '</h3><p>' + shopData.animal.description + '</p></div></a>');
+            $('#shopTable').append('<a href="#"><div id="' + shopData.animalId + '" class="shop ' + classTag + '" ><h3>' + shopData.animal.name + '</h3><p>' + shopData.animal.description + '</p></div></a>');
+            console.log(shopData.animalId);
         });
     }
 
@@ -180,6 +140,80 @@ $(document).ready(function() {
             removeShopFromList(".chipsTag");
         }
     });
+
+    /* All the buttons for switching between admin
+      sites.
+    */
+
+    $("#loggaIn").click(function() {
+        if ($('#firstView').css('display') == 'block') {
+            $("#adminLog,#firstView").toggle();
+        } else if ($('#secondView').css('display') == 'block') {
+            $('#adminLog,#secondView').toggle();
+        } else if ($('#thirdView').css('display') == 'block') {
+            $('#adminLog,#thirdView').toggle();
+        } else if ($('#forthView').css('display') == 'block') {
+            $('#adminLog,#forthView').toggle();
+        }
+    });
+
+
+    // Checks wich "section" the user is currently in and changes it to the main page.
+    $("#loggaUt").click(function() {
+        if ($("#adminMain").css('display') == 'block') {
+            $('#adminMain,#firstView').toggle();
+        } else if ($('#adminButik').css('display') == 'block') {
+            $('#adminButik,#firstView').toggle();
+        } else if ($('#adminSnacks').css('display') == 'block') {
+            $('#adminSnacks,#firstView').toggle();
+        } else if ($('#shopRemoval').css('display') == 'block') {
+            $('#shopRemoval,#firstView').toggle();
+        } else if ($('#snacksRemoval').css('display') == 'block') {
+            $('#snacksRemoval,#firstView').toggle();
+        }
+        $('#loggaUt,#loggaIn').toggle();
+    });
+
+    $('#toShops').click(function(e) {
+        e.preventDefault();
+        $('#adminMain,#adminButik').toggle();
+    });
+
+    $('#toSnacks').click(function(e) {
+        e.preventDefault();
+        $('#adminMain,#adminSnacks').toggle();
+    });
+
+    $('#removeShop').click(function(e) {
+        e.preventDefault();
+        $('#adminMain,#shopRemoval').toggle();
+    });
+
+    $('#removeSnack').click(function(e) {
+        e.preventDefault();
+        $('#adminMain,#snacksRemoval').toggle();
+    });
+
+    $("#saveShop").click(function(e) {
+        e.preventDefault();
+        $("#adminButik,#adminMain").toggle();
+    });
+
+    $("#saveSnacks").click(function(e) {
+        e.preventDefault();
+        $("#adminSnacks,#adminMain").toggle();
+    });
+
+    $('#deleteSnack').click(function(e) {
+        e.preventDefault();
+        $('#adminMain,#snacksRemoval').toggle();
+    });
+
+    $('#deleteShop').click(function(e) {
+        e.preventDefault();
+        $('#adminMain,#shopRemoval').toggle();
+    });
+
 
     /* functions for footer info
         the names should be self explanatory
